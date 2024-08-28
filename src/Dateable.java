1651f1396cc9fc5093bdc152e3090438be838cd6
samsonchan <samsonc2027@student.cis.edu.hk>
2024-08-28 09:20:20 +0800

    Completed Lab 1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);		// line 1
+        System.out.println("Please enter your age:");		// line 2
+        int age = scanner.nextInt();						// line 3
+        System.out.println(age);
+
+        int minDateAge = (age / 2) + 9;
+        System.out.printf("%d-year olds should date somebody who is at least %d years old.%n", age, minDateAge);
+
+        scanner.close();
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);		// line 1
+        System.out.println("Please enter your age:");		// line 2
+        int age = scanner.nextInt();						// line 3
+        System.out.println(age);
 
+        int minDateAge = (100 - age);
+        System.out.printf("%d-year olds should read at least %d pages before giving up on a book.%n", age, minDateAge);
+
+        scanner.close();
     }
 }
+
+
+
